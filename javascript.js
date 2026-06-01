477a6e50b2553910bc4793350f25bee4beda00b8
avanishrai,jarh <avanishr2008@gmail.com>
2026-06-01 10:38:11 +0530

    css

--- javascript.js
+++ javascript.js
@@ -54,23 +54,6 @@ function updateFlag(event){
 
 btn.addEventListener("click",async (eve)=>{
     eve.preventDefault();
-//     let amount=document.querySelector(".amount input");
-//     let amountValue=amount.value;
-//     if(amountValue<0 || amountValue===""){
-//         amountValue=1;
-//         amount.value=1;
-//     }
-//     let url=`https://2024-03-06.currency-api.pages.dev/v1/currencies/${fromCurr.value.toLowerCase()}.json`;
-// let promise=await fetch(url);
-// console.log(promise.status);
-// let newPromise=await promise.json();
-// let from=fromCurr.value.toLowerCase();
-// let to=toCurr.value.toLowerCase()
-// let fromTo=newPromise[from][to];
-// let convertedValue=amountValue*fromTo;
-// let mess=document.querySelector(".message");
-// let represent=`${amountValue}\t${fromCurr.value}\t=\t${convertedValue}\t${toCurr.value}`;
-// mess.innerText=represent;
    litstener();
 });
 
@@ -94,3 +77,39 @@ mode.onclick=()=>{
         alpha="original";
     }
 };
+
+let sp=document.querySelector(".snap");
+let side=document.querySelector(".sidebar");
+let newEle1=document.createElement("a");
+        newEle1.innerHTML='<i class="fa-solid fa-house"></i>Home';
+        newEle1.href="index.html";
+        newEle1.style.cursor="pointer";
+        newEle1.href="index.html";
+        newEle1.style.color="white";
+let newEle2=document.createElement("a");
+        newEle2.innerHTML='<i class="fa-solid fa-circle-info"></i>About';
+        newEle2.href="https://www.linkedin.com/in/avanish-rai-b68b2237b/";
+        newEle2.style.cursor="pointer";
+        newEle2.style.color="white";
+let newEle3=document.createElement("a");
+        newEle3.innerHTML='<i class="fa-solid fa-phone"></i>Contact';
+        newEle3.href="https://www.instagram.com/jarhnitian.srinagar/?utm_source=wa4a&utm_campaign=wa_vpl_m2_vf_web";
+        newEle3.style.cursor="pointer";
+        newEle3.style.color="white";
+        side.append(newEle1);
+        side.append(document.createElement("br"));
+        side.append(newEle2);
+        side.append(document.createElement("br"));
+        side.append(newEle3);
+
+let snip="close";
+sp.addEventListener("click",()=>{
+    if(snip=="close"){
+        side.style.left="0rem";
+        snip="open";
+    }
+    else{
+        side.style.left="-11rem";
+        snip="close";
+    }
+});
